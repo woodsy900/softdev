2fd181d9b69333d2c8448fa8494312d141991e77
Andrew Ingram <woodsy900@gmail.com>
2023-01-22 18:48:02 -0600

    code changes specifically to the fetch

--- car.js
+++ car.js
@@ -1,13 +1,19 @@
 //variables
 
-const makeforType = "https://vpic.nhtsa.dot.gov/api/vehicles/GetMakesForVehicleType";
+const makeforType = "https://vpic.nhtsa.dot.gov/api/vehicles/GetMakesForVehicleType/car?format=json";
 const makeList = document.getElementById("make-list");
 //when page loads
-window.addEventListener("load", );
+window.addEventListener("load", (event) => {
+    console.log("page is fully loaded");
+  });
 //retrieve list of makes 
-async function getMakelist(){
-    return fetch(makeforType).then(response => response.json());
-}
+
+fetch(makeforType)
+.then((response) => response.json())
+.then(data => {
+    console.log(data)
+})
+
 //add to dropdown list
 function updateMakeList(){
     getMakelist().then(function(data){
@@ -18,12 +24,10 @@ function updateMakeList(){
          makeList.appendChild(option);
          }
         }
-    )
-
-}
-
+    );
+};
 function creatOption(text){
     let option = document.createElement("option");
     option.textContent = text;
     return option;
-}
+};
